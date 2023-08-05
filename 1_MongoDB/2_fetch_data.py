48eaacf9a956fe06666368718028e3dcffeb6d18
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2023-08-05 20:10:41 +0530

    Nested documents
    - Json inside a json and array
    - Retrival

--- 1_MongoDB/2_fetch_data.py
+++ 1_MongoDB/2_fetch_data.py
@@ -71,5 +71,22 @@ data2 = [
 #     print(m)
 
 # 7. and operator
-for m in documents.find({"$and":[{"qualification":"CSE"},{"age":{"$lt":29}}]}):
-    print(m)
+# for m in documents.find({"$and":[{"qualification":"CSE"},{"age":{"$lt":29}}]}):
+#     print(m)
+
+# ADD NESTED DOCUMENTS
+inventory = db.inventory
+# inventory.insert_many( [
+#    { 'item': "journal", 'qty': 25, 'size': { 'h': 14, 'w': 21,'uom': "cm" }, 'status': "A" },
+#    { 'item': "notebook", 'qty': 50,'size': { 'h': 8.5, 'w': 11,'uom': "in" },'status': "A" },
+#    { 'item': "paper", 'qty': 100, 'size': { 'h': 8.5, 'w': 11,'uom': "in" },'status': "D" },
+#    { 'item': "planner", 'qty': 75, 'size': { 'h': 22.85,'w': 30,'uom': "cm" },'status': "D" },
+#    { 'item': "postcard", 'qty': 45, 'size': { 'h': 10, 'w': 15.25,'uom': "cm" },'status': "A" },
+#    { 'items': ["a", "b", "c"], 'status': "A" }
+# ])
+
+# for i in inventory.find({"size":{"h":14,"w":21,"uom":"cm"}}):
+#     print(i)
+
+for i in inventory.find({'items': ["a", "b", "c"]}):
+    print(i)
