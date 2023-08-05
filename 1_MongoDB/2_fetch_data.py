92ddd504c9d1d5ac1d7b08071c7fae69a5921095
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2023-08-05 19:59:19 +0530

    simple querying of data

---
+++ 1_MongoDB/2_fetch_data.py
@@ -0,0 +1,75 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client['Krish_tutorial_2']
+documents = db['employe_information']
+data = {
+    "First name": "Krish0",
+    "second name": "naik0",
+    "department": "Analytics",
+    "qualification": "MBA",
+    "age": 30
+}
+# documents.insert_one(data)
+
+data2 = [
+    {
+        "First name": "Karthi1",
+        "second name": "M",
+        "department": "Digital Marketing",
+        "qualification": "B.Tech",
+        "age": 21
+    },
+    {
+        "First name": "Jagan1",
+        "second name": "KSG",
+        "department": "Researcher",
+        "qualification": "M.Tech",
+        "age": 24
+    },
+    {
+        "First name": "Padmanathan1",
+        "second name": "K",
+        "department": "Analyst",
+        "qualification": "Bio-Tech",
+        "age": 23
+    },
+    {
+        "First name": "Sreejesh1",
+        "second name": "S",
+        "department": "Application testing",
+        "qualification": "CSE",
+        "age": 20
+    }
+]
+
+# documents.insert_many(data2)
+
+# SIMPLE QUERY
+# 1. To retrive single record
+# result = documents.find_one()
+# print(result)
+
+# 2. To retrive multiple record
+# for i in documents.find({}):
+#     print(i)
+    
+# 3. To retrive specific record
+# for j in documents.find({"First name": "Karthi1"}):
+#     print(j)
+
+# 4. Querying by operators
+# for k in documents.find({"qualification":{"$in":["CSE","M.Tech"]}}):
+#     print(k)
+
+# 5. Querying by and and less than
+# for l in documents.find({"qualification":{"$in":["CSE","M.Tech"]},"age":{"$lt":29}}):
+#     print(l)
+    
+# 6. or operator
+# for m in documents.find({"$or":[{"qualification":"CSE"},{"age":{"$lt":29}}]}):
+#     print(m)
+
+# 7. and operator
+for m in documents.find({"$and":[{"qualification":"CSE"},{"age":{"$lt":29}}]}):
+    print(m)
